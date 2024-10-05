d8accabfd82dcc480b20f25a28b119f502a955b5
maulida34 <maulidarizky882@gmail.com>
2024-10-05 17:36:35 +0800

    menambahkan project laravel ke git

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+use Illuminate\View\View;
+
+use Illuminate\Http\RedirectResponse;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function index() : View
+    {
+        //get all products
+        $products = Product::latest()->paginate(10);
+
+        //render view with products
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * create
+     * 
+     * @return view
+     */
+    public function create():View
+    {
+        return view('products.create');
+    }
+
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        //$image->storeAs('public/storage', $image->hashName());
+        $image->storeAs ('products',$image->hashName(), 'public');
+
+        //create product
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success'=>'Data Berhasil Disimpan!']);
+    }
+
+    public function show(string $id): View
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+        
+        //render view with product
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(string $id): View
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //render view with product
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * update
+     * 
+     * @param mixed $request
+     * @param mixed $id
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //get product by ID
+        $product = Product::findOrFail($id);
+        
+        //check if image is uploaded
+        if  ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/products/'.$product->image);
+
+            //update product with new image
+            $product->update([
+              'image'         => $image->hashName(),
+              'title'         => $request->title,
+              'description'   => $request->description,
+              'price'         => $request->price,
+             'stock'         => $request->stock
+            ]);
+
+        } else {
+
+            //update product without new image
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+               'stock'         => $request->stock
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success'=>'Data Berhasil Diubah!']);
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //delete image
+        Storage::delete('public/products/'.$product->image);
+
+        //delete product
+        $product->delete();
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success'=>'Data Berhasil Dihapus!']);
+    }
+}
